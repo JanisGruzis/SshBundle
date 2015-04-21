f198c980841d8a01a3118d38d4f1c5f8125c45a9
Jānis Gruzis <jgruzis@gmail.com>
2015-04-21 12:29:19 +0300

    Tests added

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JanisGruzis\SshBundle\Tests\Controller;
+
+use JanisGruzis\SshBundle\DependencyInjection\Configuration;
+use JanisGruzis\SshBundle\DependencyInjection\JanisGruzisSshExtension;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetConfigTreeBuilder()
+    {
+    }
+}
